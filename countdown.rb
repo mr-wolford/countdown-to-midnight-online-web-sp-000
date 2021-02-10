1b1143097eb70a21d14e3d0c037dcb8dfeb25165
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-10 01:56:59 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(counter)
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(counter)
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
 end
