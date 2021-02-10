9e8d899501376b91d3d8fa3e22a312e80fb3262d
Matt Wolford <matthew.r.wolford@gmail.com>
2021-02-10 01:44:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,9 @@
-#write your code here
-
-def countdown
+def countdown(counter)
+  while counter > 1
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+  end
+  if counter == 1
+    puts "HAPPY NEW YEAR!"
+  end
 end
